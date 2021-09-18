21454e6e7bf5c3879ba6acc07652e967318b4d96
YBenPan <ybenp8104@outlook.com>
2021-09-17 23:19:56 -0400

    Add images of averages of first 10 and last 10

--- main.py
+++ main.py
@@ -9,13 +9,18 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
-import datetime
+from datetime import datetime
 from density import get_density
 
 # Paths Settings
 mmrpm2p5_path = "D:/PM2.5"
 density_path = "F:/Computer Programming/Projects/CMIP6/data/"
 output_image_path = "D:/PM2.5_images"
+
+def year_helper(date):
+    if isinstance(date, np.datetime64):
+        date = pd.to_datetime(date)
+    return date.year
 
 # Generate density data for conversion
 # get_density(density_path)
@@ -39,6 +44,9 @@ for ssp in ssps:
         reals = os.listdir(f"{mmrpm2p5_path}/{ssp}/mmrpm2p5/{model}")
         for real in reals:
 
+            if (ssp != "ssp370" or model != "MIROC-ES2L"): 
+                continue
+
             # Loop over glabels
             glabel = os.listdir(f"{mmrpm2p5_path}/{ssp}/mmrpm2p5/{model}/{real}")
             data_dir = f"{mmrpm2p5_path}/{ssp}/mmrpm2p5/{model}/{real}/{glabel[0]}"
@@ -53,7 +61,7 @@ for ssp in ssps:
 
             # Annualize
             annual_ds = ds.resample(time="1YS").mean()
-
+            
             # Regrid
             new_lat = np.arange(-90, 90, 0.5)
             new_lon = np.arange(0, 360, 0.5)
@@ -68,27 +76,43 @@ for ssp in ssps:
             dsi.mmrpm2p5.values *= density_matrix
             dsi = dsi.rename({"mmrpm2p5": "concpm2p5"})
 
+            print(dsi.concpm2p5.values)
+            # Initialize output directories
             output_image_dir = (
                 f"{output_image_path}/{ssp}/mmrpm2p5/{model}/{real}/{glabel[0]}/"
             )
             output_image_file_name = "result"
             if not os.path.isdir(output_image_dir):
-                os.makedirs(output_image_dir)
+                os.makedirs(output_image_dir)            
 
             # Plot last datapoint in time and save the figure
-            fig, axis = plt.subplots(subplot_kw={"projection": ccrs.PlateCarree()})
-            dsi.concpm2p5.isel(time=len(dsi.concpm2p5.time) - 1).plot(
-                ax=axis,
-                transform=ccrs.PlateCarree(),
-                cbar_kwargs={"label": "Concentration of PM2.5 in kg $m^{-3}$"},
-            )
+            fig, axes = plt.subplots(nrows=2, subplot_kw={"projection": ccrs.PlateCarree()})
             fig.set_size_inches(12, 8)
-            axis.coastlines()
-            output_date = dsi.concpm2p5.time.values[-1]
-            plt.title(f"{ssp}, model {model}, realization {real} on {output_date}")
-            plt.savefig(output_image_dir + output_image_file_name)
-            plt.close(fig)
-            print(
-                f"{datetime.datetime.now()} DONE: {ssp}, model {model}, realization {real}"
+            
+            avg_first_10 = np.mean(dsi.concpm2p5.values[:10], axis=0)
+            avg_last_10 = np.mean(dsi.concpm2p5.values[-10:], axis=0)
+
+            im = axes[0].pcolormesh(dsi.lon.values, dsi.lat.values, avg_first_10[0])
+            im = axes[1].pcolormesh(dsi.lon.values, dsi.lat.values, avg_last_10[0])
+            fig.colorbar(im, ax=axes.ravel().tolist(), label="Concentration of PM2.5 in kg $m^{-3}$")
+
+            start_year = year_helper(dsi.concpm2p5.time.values[0])
+            end_year = year_helper(dsi.concpm2p5.time.values[9])
+            axes[0].coastlines()
+            axes[0].set_title(
+                f"Average of PM2.5 Concentration from {start_year} to {end_year}\n"
+                f"{ssp}, model {model}, realization {real}"
             )
-            # plt.show()
+            axes[1].coastlines()
+            end_year = year_helper(dsi.concpm2p5.time.values[-1])
+            start_year = year_helper(dsi.concpm2p5.time.values[-10])
+            axes[1].set_title(
+                f"Average of PM2.5 Concentration from {start_year} to {end_year}\n"
+                f"{ssp}, model {model}, realization {real}"
+            )
+            # plt.savefig(output_image_dir + output_image_file_name)
+            print(
+                f"{datetime.now()} DONE: {ssp}, model {model}, realization {real}"
+            )
+            plt.show()
+            plt.close(fig)
