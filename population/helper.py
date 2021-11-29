1ce5d70c27e5721c7de80a21ceccbaf08af6a097
YBenPan <ybenp8104@outlook.com>
2021-11-28 20:17:32 -0500

    Minor Changes

--- population/helper.py
+++ population/helper.py
@@ -1,164 +1,162 @@
 from netCDF4 import Dataset
 import numpy as np
 import pandas as pd
-import csv
 import xarray as xr
 
 from pandas.core.indexes import multi
 
 ####################################################################################################
+#### REMOVES, RENAMES, AND ADDS COUNTRIES FROM THE POPULATION DATA (WITH AGE GROUPS)
+#### SO THAT THE NEW DATA HAS EXACTLY 193 NATIONS WITH CORRECT NAMES.
+####################################################################################################
+
+####################################################################################################
 #### 05x05 COUNTRY FRACTIONS (PUT THE 6 FILES TOGETHER)
 #### USER INPUT:
 #### WHERE IS THE COUNTRY LEVEL .CSV FILE?
-country_path = 'F:\\Computer Programming\\Projects\\CMIP6\\data\\population\\'
-#### WHAT'S THE NAME OF THE COUNTRY LEVEL BASELINE FILE?
-country_file = 'wcde_data_SSP1.csv'
+country_path = "F:\\Computer Programming\\Projects\\CMIP6\\data\\population\\"
+#### WHAT ARE THE NAMES OF THE COUNTRY LEVEL BASELINE FILE?
+country_files = ["wcde_data_SSP1.csv", "wcde_data_SSP2.csv", "wcde_data_SSP3.csv"]
 #### WHERE IS THE COUNTRY NAME .CSV FILE?
-name_path = 'F:\\Computer Programming\\Projects\\CMIP6\\data\\population\\'
+name_path = "F:\\Computer Programming\\Projects\\CMIP6\\data\\population\\"
 #### WHAT'S THE NAME OF THE COUNTRY NAME FILE?
-name_file = 'countryvalue_blank.csv'
+name_file = "countryvalue_blank.csv"
 #### Where to save the output
-output_path = 'F:\\Computer Programming\\Projects\\CMIP6\\data\\population\\'
+output_path = "F:\\Computer Programming\\Projects\\CMIP6\\data\\population\\"
+#### WHAT ARE THE NAMES OF THE OUTPUT FILES?
+output_files = ["SSP1.nc", "SSP2.nc", "SSP3.nc"]
 ####################################################################################################
 
-# Note to self:
-# 1. Should country_name be index of dataframe? 
-# 2. Should country_id be 0-based or 1-based?
-# 3. Order of sorting/columns
+for ssp_index in range(len(country_files)):
 
-df = pd.read_csv(country_path + country_file, header=7)
+    df = pd.read_csv(country_path + country_files[ssp_index], header=7)
 
-# Add countries
+    # Add countries
 
-to_be_added = [
-    "Andorra",
-    "Cook Islands",
-    "Dominica",
-    "Marshall Islands",
-    "Monaco",
-    "Nauru",
-    "Niue",
-    "Palau",
-    "San Marino",
-    "St.Kitts+Nevis",
-    "Tuvalu",
-]
+    to_be_added = [
+        "Andorra",
+        "Cook Islands",
+        "Dominica",
+        "Marshall Islands",
+        "Monaco",
+        "Nauru",
+        "Niue",
+        "Palau",
+        "San Marino",
+        "St.Kitts+Nevis",
+        "Tuvalu",
+    ]
 
-years = df['Year'].drop_duplicates().tolist()
+    years = df["Year"].drop_duplicates().tolist()
+    ages = df["Age"].drop_duplicates().tolist()
 
-ages = df['Age'].drop_duplicates().tolist()
+    new_rows = [
+        pd.Series([area, year, age, 0], index=df.columns)
+        for area in to_be_added
+        for year in years
+        for age in ages
+    ]
+    df = df.append(new_rows)
 
-new_rows = [pd.Series([area, year, age, 0], index=df.columns) for area in to_be_added for year in years for age in ages]
+    # Combine Serbia and Montenegro
 
-df = df.append(new_rows)
+    Serbia_index = [x for x in df.index.values if df["Area"].iloc[x] == "Serbia"]
+    Montenegro_index = [
+        x for x in df.index.values if df["Area"].iloc[x] == "Montenegro"
+    ]
+    if len(Serbia_index) != len(Montenegro_index):
+        raise Exception(
+            "Number of data points of Serbia is not equal to that of Montenegro!"
+        )
+    for i in range(0, len(Serbia_index)):
+        df.at[Serbia_index[i], "Population"] += df.at[Montenegro_index[i], "Population"]
 
-# Combine Serbia and Montenegro
+    # Drop Countries
 
-Serbia_index = [x for x in df.index.values if df['Area'].iloc[x] == 'Serbia']
+    to_be_dropped = [
+        "Aruba",
+        "Channel Islands",
+        "Curaçao",
+        "French Guiana",
+        "French Polynesia",
+        "Guadeloupe",
+        "Guam",
+        "Hong Kong Special Administrative Region of China",
+        "Macao Special Administrative Region of China",
+        "Martinique",
+        "Mayotte",
+        "Montenegro",
+        "New Caledonia",
+        "Occupied Palestinian Territory",
+        "Reunion",
+        "South Sudan",
+        "Taiwan Province of China",
+        "United States Virgin Islands",
+        "Western Sahara",
+        "World",
+    ]
 
-Montenegro_index = [x for x in df.index.values if df['Area'].iloc[x] == 'Montenegro']
+    countries = df["Area"].drop_duplicates().tolist()
+    countries = [x for x in countries if x not in to_be_dropped]
 
-if (len(Serbia_index) != len(Montenegro_index)):
-    raise Exception("Number of data points of Serbia is not equal to that of Montenegro!")
+    df = df.set_index("Area")
+    df = df.loc[countries]
 
-for i in range(0, len(Serbia_index)):
-    df.at[Serbia_index[i], 'Population'] += df.at[Montenegro_index[i], 'Population']
+    # Rename countries
 
+    to_be_renamed = {
+        "Bahamas": "The Bahamas",
+        "Bolivia (Plurinational State of)": "Bolivia",
+        "Brunei Darussalam": "Brunei",
+        "Democratic People's Republic of Korea": "North Korea",
+        "Gambia": "The Gambia",
+        "Iran (Islamic Republic of)": "Iran",
+        "Lao People's Democratic Republic": "Laos",
+        "Libyan Arab Jamahiriya": "Libya",
+        "Micronesia (Federated States of)": "Federated States of Micronesia",
+        "Republic of Korea": "South Korea",
+        "Republic of Moldova": "Moldova",
+        "Russian Federation": "Russia",
+        "Serbia": "Serbia+Montenegro",
+        "Syrian Arab Republic": "Syria",
+        "The former Yugoslav Republic of Macedonia": "Macedonia",
+        "United Kingdom of Great Britain and Northern Ireland": "United Kingdom",
+        "United Republic of Tanzania": "Tanzania",
+        "United States of America": "United States",
+        "Venezuela (Bolivarian Republic of)": "Venezuela",
+        "Viet Nam": "Vietnam",
+    }
 
-# Drop Countries
+    labels = df.index.values
+    new_labels = list(
+        map(lambda x: to_be_renamed[x] if x in to_be_renamed else x, labels)
+    )
 
-to_be_dropped = [
-    "Aruba",
-    "Channel Islands",
-    "Curaçao",
-    "French Guiana",
-    "French Polynesia",
-    "Guadeloupe",
-    "Guam",
-    "Hong Kong Special Administrative Region of China",
-    "Macao Special Administrative Region of China",
-    "Martinique",
-    "Mayotte",
-    "Montenegro",
-    "New Caledonia",
-    "Occupied Palestinian Territory",
-    "Reunion",
-    "South Sudan",
-    "Taiwan Province of China",
-    "United States Virgin Islands",
-    "Western Sahara",
-    "World",
-]
+    df = df.set_axis(new_labels, axis="index")
+    df.index.name = "Area"
 
+    # Import country ids
 
-countries = df['Area'].drop_duplicates().tolist()
+    countries_df = pd.read_csv(name_path + name_file, header=0)
 
-countries = [x for x in countries if x not in to_be_dropped]
+    country_names = list(countries_df["COUNTRY"])
+    country_ids = list(countries_df["COUNTRY_NO"] - 1)
+    country_dict = dict(zip(country_names, country_ids))
 
-df = df.set_index('Area')
+    # Create country id column and Sort
 
-df = df.loc[countries]
+    new_country_ids = [country_dict[x] for x in df.index.values]
+    if len(new_country_ids) != len(df):
+        raise Exception("Length of country IDs is not equal to length of dataframe!")
+    df.insert(loc=0, column="Country_ID", value=new_country_ids)
 
-# Rename countries
+    df.reset_index()
 
-to_be_renamed = {
-    "Bahamas": "The Bahamas",
-    "Bolivia (Plurinational State of)": "Bolivia",
-    "Brunei Darussalam": "Brunei",
-    "Democratic People's Republic of Korea": "North Korea",
-    "Gambia": "The Gambia",
-    "Iran (Islamic Republic of)": "Iran",
-    "Lao People's Democratic Republic": "Laos",
-    "Libyan Arab Jamahiriya": "Libya",
-    "Micronesia (Federated States of)": "Federated States of Micronesia",
-    "Republic of Korea": "South Korea",
-    "Republic of Moldova": "Moldova",
-    "Russian Federation": "Russia",
-    "Serbia": "Serbia+Montenegro",
-    "Syrian Arab Republic": "Syria",
-    "The former Yugoslav Republic of Macedonia": "Macedonia",
-    "United Kingdom of Great Britain and Northern Ireland": "United Kingdom",
-    "United Republic of Tanzania": "Tanzania",
-    "United States of America": "United States",
-    "Venezuela (Bolivarian Republic of)": "Venezuela",
-    "Viet Nam": "Vietnam",
-}
+    df = df.sort_values(by=["Year", "Age", "Country_ID"])
+    multi_df = df.set_index(["Year", "Age", "Country_ID"])
+    ds = multi_df.to_xarray()
 
-labels = df.index.values
+    # Output
 
-new_labels = list(map(lambda x: to_be_renamed[x] if x in to_be_renamed else x, labels))
-
-df = df.set_axis(new_labels, axis='index')
-
-df.index.name = "Area"
-
-# Import country ids
-
-countries_df = pd.read_csv(name_path + name_file, header=0)
-
-country_names = list(countries_df['COUNTRY'])
-
-country_ids = list(countries_df['COUNTRY_NO'] - 1)
-
-country_dict = dict(zip(country_names, country_ids))
-
-# Create country id column and Sort
-
-new_country_ids = [country_dict[x] for x in df.index.values]
-
-if (len(new_country_ids) != len(df)):
-    raise Exception("Length of country IDs is not equal to length of dataframe!")
-
-df.insert(loc=0, column="Country_ID", value=new_country_ids)
-
-df.reset_index()
-
-df = df.sort_values(by = ['Year', 'Age', 'Country_ID'])
-
-multi_df = df.set_index(['Year', 'Age', 'Country_ID'])
-
-ds = multi_df.to_xarray()
-
-ds.to_netcdf(output_path + 'SSP1.nc')
-
-multi_df.to_csv(output_path + 'SSP1.csv')
+    ds.to_netcdf(output_path + output_files[ssp_index])
+    ds.close()
--- population/pop.py
+++ population/pop.py
@@ -1,170 +1,116 @@
 # -*- coding: utf-8 -*-
 from netCDF4 import Dataset
 import numpy as np
-import pandas as pd
 import os
 import xarray as xr
+from datetime import datetime
 
 ####################################################################################################
-#### CONVERTS THE .NPY FILES WITH COUNTRY LEVEL DATA TO A 0.5X0.5 DEGREE GLOBAL NETCDF FILE
-#### THE ORDER OF THE COUNTRIES IN THE .NPY FILE IS FIXED AND CANNOT BE CHANGED
+#### CALCULATES POPULATION DATA OF DIFFERENT AGE GROUPS (0-19, 20-39, 40-64, 65+) IN A FUTURE YEAR (2010, 2020, ... 2100)
+#### PROJECTS DATA ONTO A 0.5X0.5 DEGREE GRID
 ####################################################################################################
 
 ####################################################################################################
 #### 05x05 COUNTRY FRACTIONS (PUT THE 6 FILES TOGETHER)
 #### USER INPUT:
-year = '2010' ## '2005' and '2016'
-#### WHERE IS THE 0.1X0.1 DEGREE FILE WITH COUNTRY FRACTIONS?
-base_path = 'F:\\Computer Programming\\Projects\\CMIP6\\data\\population\\'
+#### WHERE IS THE 0.5X0.5 DEGREE FILE WITH COUNTRY FRACTIONS?
+base_path = "F:\\Computer Programming\\Projects\\CMIP6\\data\\population\\"
 #### WHAT IS THE NAME OF THE FILE WITH THE COUNTRY FRACTIONS?
 #### IF 05X05
-base_file = 'countryFractions_2010_0.5x0.5.nc'
-#### IF 01X01
-#base_file = 'CountryFractions_2010_0.1x0.1/countryFractions_2010_01x01_'
+base_file = "countryFractions_2010_0.5x0.5.nc"
 #### WHERE IS THE COUNTRY LEVEL .CSV FILE?
-country_path = 'F:\\Computer Programming\\Projects\\CMIP6\\data\\population\\'
+country_path = "F:\\Computer Programming\\Projects\\CMIP6\\data\\population\\"
 #### WHAT'S THE NAME OF THE COUNTRY LEVEL BASELINE FILE?
-country_file = 'SSP1.nc'
-#### WHERE ARE THE FILES WITH GRIDDED POPULATION? 
-pop_path = 'D:\\CMIP6_data\\Pop\\'
+country_file = ["SSP1.nc", "SSP2.nc", "SSP3.nc"]
+#### WHERE ARE THE FILES WITH GRIDDED POPULATION?
+pop_path = "D:\\CMIP6_data\\Pop\\"
 #### Where to save the output
 output_path = "D:\\CMIP6_data\\Pop_Result"
 ####################################################################################################
 
 #### IMPORT COUNTRY FRACTIONS
-#### IF 05X05
-f1 = Dataset(base_path+base_file,'r')
-fractionCountry = f1.variables['fractionCountry'][:,:,:] # countryIndex, latitude, longitude
-latitude  = f1.variables['latitude'][:]
-longitude = f1.variables['longitude'][:]
+f1 = Dataset(base_path + base_file, "r")
+fractionCountry = f1.variables["fractionCountry"][:, :, :]  # countryIndex, latitude, longitude
+latitude = f1.variables["latitude"][:]
+longitude = f1.variables["longitude"][:]
 f1.close()
 
-# #### IF 01x01 
-# fractionCountry = np.zeros(shape = (193,1800,3600)) ## 193 is the number of countries 
-
-#region
-# f1              = Dataset(base_path+base_file+'a.nc', 'r')
-# fractionCountry[0:25,:,:] = f1.variables['fractionCountry'][:,:,:]   
-# latitude  = f1.variables['lat'][:]
-# longitude = f1.variables['lon'][:]
-# #num_countries   = len(f1.variables['countryIndex'][:])   ## country index starts from 1
-# f1.close()
-# f1              = Dataset(base_path+base_file+'b.nc', 'r')
-# fractionCountry[25:50,:,:] = f1.variables['fractionCountry'][:,:,:]   
-# #num_countries   = num_countries+len(f1.variables['countryIndex'][:])   ## country index stack on the previous one
-# f1.close()
-# f1              = Dataset(base_path+base_file+'c.nc', 'r')
-# fractionCountry[50:75,:,:] = f1.variables['fractionCountry'][:,:,:] 
-# #num_countries   = num_countries+len(f1.variables['countryIndex'][:])   
-# f1.close()
-# f1              = Dataset(base_path+base_file+'d.nc', 'r')
-# fractionCountry[75:100,:,:] = f1.variables['fractionCountry'][:,:,:] 
-# #num_countries   = num_countries+len(f1.variables['countryIndex'][:])   
-# f1.close()
-# f1              = Dataset(base_path+base_file+'e.nc', 'r')
-# fractionCountry[100:125,:,:] = f1.variables['fractionCountry'][:,:,:] 
-# #num_countries   = num_countries+len(f1.variables['countryIndex'][:])   
-# f1.close()
-# f1              = Dataset(base_path+base_file+'f.nc', 'r')
-# fractionCountry[125:150,:,:] = f1.variables['fractionCountry'][:,:,:] 
-# #num_countries   = num_countries+len(f1.variables['countryIndex'][:])   
-# f1.close()
-# f1              = Dataset(base_path+base_file+'g.nc', 'r')
-# fractionCountry[150:175,:,:] = f1.variables['fractionCountry'][:,:,:]  
-# #num_countries   = num_countries+len(f1.variables['countryIndex'][:])   
-# f1.close()
-# f1              = Dataset(base_path+base_file+'h.nc', 'r')
-# fractionCountry[175:194,:,:] = f1.variables['fractionCountry'][:,:,:] 
-# #num_countries   = num_countries+len(f1.variables['countryIndex'][:])   
-# f1.close()
-#endregion
-
-fractionCountry[fractionCountry<0.0] = 0.0
-fractionCountry[fractionCountry>1.0] = 0.0
+fractionCountry[fractionCountry < 0.0] = 0.0
+fractionCountry[fractionCountry > 1.0] = 0.0
 
 # Change Longitude from -180 to 180 to 0 to 360 for ease of computation
-fractionCountry = np.concatenate([fractionCountry[:,:,len(longitude) // 2:], fractionCountry[:,:, :len(longitude) // 2]], axis=2)
+fractionCountry = np.concatenate(
+    [
+        fractionCountry[:, :, len(longitude) // 2 :],
+        fractionCountry[:, :, : len(longitude) // 2],
+    ],
+    axis=2,
+)
+longitude = np.arange(0.25, 360, 0.5)
 
-longitude=(longitude + 180) % 360 - 180
+####################################################################################################
 
-####################################################################################################    
-#### IMPORT BASELINE NP ARRAYS
-#### DIMENSION OF NP ARRAYS: [4, 193] FOR age_id, location_id
-#### ALL VALUES ARE RATE PER 100K
+ssps = ["ssp1", "ssp2", "ssp3"]
+age_groups = [1, 3, 6, 8]
+age_group_names = np.array(["0--19", "20--39", "40--64", "65+"], dtype=object)
 
-ssps = ['ssp1']
-age_groups = [1, 3, 6, 8] 
-# 1: 0--19
-# 2: 20--39
-# 3: 40--64
-# 4: 65+
-years = np.arange(2010, 2020, 10)
+years = np.arange(2010, 2110, 10)
 
-data   = country_path+country_file
-country_pop_ds = xr.open_dataset(data)
-country_pop_all_age_values = country_pop_ds.data_vars['Population'].values
-country_pop_ds.close()
+for ssp in range(len(ssps)):
 
-# Select age groups
-# Shape: Year, Age Group, Country ID
-country_pop_values = country_pop_all_age_values[:,[1,3,6,8],:]
+    data = country_path + country_file[ssp]
+    country_pop_ds = xr.open_dataset(data)
+    country_pop_all_age_values = country_pop_ds.data_vars["Population"].values
+    country_pop_ds.close()
 
-# Total Population of a Country: 
-country_pop_sum = np.sum(country_pop_values, axis=1)
+    # Select age groups
+    # Shape: Year, Age Group, Country ID
+    country_pop_values = country_pop_all_age_values[:, [1, 3, 6, 8], :]
 
-for ssp in ssps:
+    # Total Population of a Country:
+    country_pop_sum = np.sum(country_pop_values, axis=1)
+
     for year in range(len(years)):
         # Import gridded population data
-        grid_pop_file_name = f'{ssp}_tot_{str(years[year])}.nc'
-        grid_pop_ds = xr.open_dataset(f'{pop_path}\\{ssp}\\{grid_pop_file_name}')
-        grid_pop_values = grid_pop_ds.data_vars['population'].values 
+        grid_pop_file_name = f"{ssps[ssp]}_tot_{str(years[year])}.nc"
+        grid_pop_ds = xr.open_dataset(f"{pop_path}\\{ssps[ssp]}\\{grid_pop_file_name}")
+        grid_pop_values = grid_pop_ds.data_vars["population"].values
         grid_pop_ds.close()
 
-        pop_array = np.zeros(shape = (len(latitude), len(longitude), len(age_groups)))
+        # Calculate and fill in the new array
+        pop_array = np.zeros(shape=(len(latitude), len(longitude), len(age_groups)))
         for age in range(len(age_groups)):
 
-            for i in range(len(country_pop_values[year][age])):
-                # print(i)
+            for i in range(len(country_pop_values[year][age])): # Number of countries
 
-                ratio = country_pop_values[year, age, i] / country_pop_sum[year, i] if country_pop_sum[year, i] != 0 else 0
+                ratio = (
+                    country_pop_values[year, age, i] / country_pop_sum[year, i]
+                    if country_pop_sum[year, i] != 0
+                    else 0
+                )
 
-                pop_array[:,:,age] += fractionCountry[i,:,:] * grid_pop_values[:,:] * ratio
-                
-                ''' # Need to verify further
-                if (i == 165):
-                    for lat in range(360):
-                        for lon in range(720):
-                            if (fractionCountry[i][lat][lon] != 0):
-                                print(
-                                    -89.75 + 0.5 * lat, 
-                                    0.25 + 0.5 * lon, 
-                                    fractionCountry[i][lat][lon], grid_pop_values[lat][lon], 
-                                    sep=' ')
-                '''
+                pop_array[:, :, age] += (
+                    fractionCountry[i, :, :] * grid_pop_values[:, :] * ratio
+                )
 
-        longitude = np.arange(0.25, 360, 0.5)
-        
-        da = xr.DataArray(
-            data=pop_array,
-            #dims=["lat", "lon", "age_group"],
+        # Create Dataset
+        ds = xr.Dataset(
+            data_vars=dict(
+                population=(["lat", "lon", "age_group"], pop_array),
+            ),
             coords=dict(
                 lat=latitude,
                 lon=longitude,
-                age_group=["0--19", "20--39", "40--64", "65+"],
-            )
+                age_group=age_group_names,
+            ),
+            attrs=dict(
+                description="Population of different age groups converted to a 0.5x0.5 degree grid",
+            ),
         )
 
         # Output
+        os.makedirs(f"{output_path}\\{ssps[ssp]}", exist_ok=True)
+        ds.to_netcdf(f"{output_path}\\{ssps[ssp]}\\{str(years[year])}.nc")
+        ds.close()
 
-        os.makedirs(f'{output_path}\\{ssp}', exist_ok=True)
-        
-        da.to_netcdf(f'{output_path}\\{ssp}\\{str(year)}.nc')
-        da.close()
-
-# print(country_pop_values[24, 0, 35] / country_pop_sum[24, 35])
-
-
-        
-
-
-
+        print(f"{datetime.now()} DONE: {ssps[ssp]}, {years[year]}")
