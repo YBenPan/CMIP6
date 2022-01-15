499c51ae73ea096db8ca38d7c5f9d6ba92825343
YBenPan <ybenp8104@outlook.com>
2022-01-15 11:08:08 -0500

    Add multiprocessing to pie and exclude models that don't go up to 2100.

--- plots/pie.py
+++ plots/pie.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import multiprocessing
 
 ####################################################################################################
 #### CREATE PIE CHARTS BASED ON COUNTRY MORTALITY DATA
@@ -35,66 +36,97 @@ year_bins = [
 
 year_bin_names = [str(x) + '0s' for x in np.arange(201, 211)]
 
-diseases = ["Allcause", "COPD", "IHD", "LC", "LRI", "Stroke", "T2D"]
-disease_labels = ["Others", "COPD", "IHD", "LC", "LRI", "Stroke", "T2D"]
+diseases = ["COPD", "IHD", "LC", "LRI", "Stroke", "T2D"]
 # diseases = ["COPD"]
 
 countries = [183, 77, 35, 193]
 
+excludes = [
+    "ssp245\\MRI-ESM2-0",
+    "ssp370\\EC-Earth3-AerChem",
+    "ssp370\\IPSL-CM5A2-INCA",
+    "ssp370\\MPI-ESM-1-2-HAM",
+    "ssp370\\NorESM2-LM"
+]
+
+
+def process(t):
+    ssp = t[0]
+    year_bin_ind = t[1]
+    mortality_path = f'{base_path}{ssp}/'
+    mortality_files = glob(f'{mortality_path}*')
+
+    year_bin = year_bins[year_bin_ind]  # 0, 1, 2, 3, 4 ...
+    year_bin_str = [str(x + 2015) for x in year_bin]  # "2015", "2016" ...
+
+    fig = plt.figure(figsize=(15, 15))
+    axes = fig.subplots(nrows=2, ncols=2)
+    axes[0, 0].set_title("USA")
+    axes[0, 1].set_title("India")
+    axes[1, 0].set_title("China")
+    axes[1, 1].set_title("World")
+    title = f'Mortality Distribution in the {year_bin_names[year_bin_ind]} in {ssp} Scenario'
+    fig.suptitle(title)
+
+    data = np.empty((len(diseases), len(countries)))
+
+    for disease_ind in range(len(diseases)):
+        disease = diseases[disease_ind]
+        # Choose all files with correct disease and year within the year bin
+        disease_files = [x for x in mortality_files if
+                         disease in x
+                         and any(year in x for year in year_bin_str)
+                         and not any(exclude in x for exclude in excludes)
+                         ]
+        # print(*disease_files, sep='\n')
+
+        # Import and sum over all files
+        column = 13 if disease in ["Allcause", "IHD", "Stroke"] else 1
+        mortality = [np.genfromtxt(x, delimiter=',', usecols=column) for x in disease_files]
+        mortality = np.sum(mortality, axis=0)
+        mortality /= len(disease_files)
+
+        # Select USA, India, China, and World
+        mortality = [mortality[183], mortality[77], mortality[35], mortality[0:].sum()]
+        data[disease_ind] = mortality
+
+    axes[0, 0].pie(data[:, 0], labels=diseases, autopct='%.1f%%')
+    axes[0, 1].pie(data[:, 1], labels=diseases, autopct='%.1f%%')
+    axes[1, 0].pie(data[:, 2], labels=diseases, autopct='%.1f%%')
+    axes[1, 1].pie(data[:, 3], labels=diseases, autopct='%.1f%%')
+
+    output_file = f'{output_path}{ssp}/{year_bin_names[year_bin_ind]}.png'
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)  # Create directory if it does not exist
+    plt.savefig(output_file)
+    # plt.show()
+    plt.close(fig)
+
+    print(f'DONE: {ssp}, {year_bin_names[year_bin_ind]}, {datetime.now()}')
+
 
 def pie():
     for ssp in ssps:
+        pool = multiprocessing.Pool(5)
+        pool.map(process, [(ssp, year_bin_ind) for year_bin_ind in range(len(year_bins))])
+        pool.close()
+        pool.join()
+
+
+def diff():
+    for ssp in ssps:
 
         mortality_path = f'{base_path}{ssp}/'
-        mortality_files = glob(f'{mortality_path}*')
+        all_files = glob(mortality_path + "*")
+        mortality_files = sorted(glob(mortality_path + "*" + str(2015) + "_Allcause_CountryMortalityAbsolute_GEMM.csv"))
 
-        for year_bin_ind in range(len(year_bins)):
-            year_bin = year_bins[year_bin_ind]  # 0, 1, 2, 3, 4 ...
-            year_bin_str = [str(x + 2015) for x in year_bin]  # "2015", "2016" ...
-
-            fig = plt.figure(figsize=(15, 15))
-            axes = fig.subplots(nrows=2, ncols=2)
-            axes[0, 0].set_title("USA")
-            axes[0, 1].set_title("India")
-            axes[1, 0].set_title("China")
-            axes[1, 1].set_title("World")
-            title = f'Mortality Distribution in the {year_bin_names[year_bin_ind]} in {ssp} Scenario'
-            fig.suptitle(title)
-
-            data = np.empty((len(diseases), len(countries)))
-
-            for disease_ind in range(len(diseases)):
-                disease = diseases[disease_ind]
-                # Choose all files with correct disease and year within the year bin
-                disease_files = [x for x in mortality_files if disease in x and any(year in x for year in year_bin_str)]
-
-                # Import and sum over all files
-                column = 13 if disease in ["Allcause", "IHD", "Stroke"] else 1
-                mortality = [np.genfromtxt(x, delimiter=',', usecols=column) for x in disease_files]
-                mortality = np.sum(mortality, axis=0)
-                mortality /= len(disease_files)
-
-                # Select USA, India, China, and World
-                mortality = [mortality[183], mortality[77], mortality[35], mortality[0:].sum()]
-                data[disease_ind] = mortality
-
-            data[0, :] -= data[1:, :].sum(axis=0)
-
-            axes[0, 0].pie(data[:, 0], labels=disease_labels, autopct='%.1f%%')
-            axes[0, 1].pie(data[:, 1], labels=disease_labels, autopct='%.1f%%')
-            axes[1, 0].pie(data[:, 2], labels=disease_labels, autopct='%.1f%%')
-            axes[1, 1].pie(data[:, 3], labels=disease_labels, autopct='%.1f%%')
-
-            output_file = f'{output_path}{ssp}/{year_bin_names[year_bin_ind]}.png'
-            os.makedirs(os.path.dirname(output_file), exist_ok=True)  # Create directory if it does not exist
-            plt.savefig(output_file)
-            # plt.show()
-            plt.close(fig)
-
-            print(f'DONE: {ssp}, {year_bin_names[year_bin_ind]}, {datetime.now()}')
+        for file in mortality_files:
+            new_file = file.replace("2015", "2100")
+            if new_file not in all_files:
+                print(ssp, file, sep=' ')
 
 
 def main():
+    # diff()
     pie()
 
 
