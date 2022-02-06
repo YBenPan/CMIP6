9450919719c437acc05cb57c8924ff52b79967a1
YBenPan <ybenp8104@outlook.com>
2022-02-05 23:48:14 -0500

    Add "all" age group. Fixed year error

--- population/pop.py
+++ population/pop.py
@@ -16,11 +16,10 @@ from datetime import datetime
 #### WHERE IS THE 0.5X0.5 DEGREE FILE WITH COUNTRY FRACTIONS?
 base_path = "F:\\Computer Programming\\Projects\\CMIP6\\data\\population\\"
 #### WHAT IS THE NAME OF THE FILE WITH THE COUNTRY FRACTIONS?
-#### IF 05X05
 base_file = "countryFractions_2010_0.5x0.5.nc"
 #### WHERE IS THE COUNTRY LEVEL .CSV FILE?
 country_path = "F:\\Computer Programming\\Projects\\CMIP6\\data\\population\\"
-#### WHAT'S THE NAME OF THE COUNTRY LEVEL BASELINE FILE?
+#### WHAT'S THE NAME OF THE COUNTRY LEVEL POPULATION FILE WITH AGE GROUPS?
 country_file = ["SSP1.nc", "SSP2.nc", "SSP3.nc"]
 #### WHERE ARE THE FILES WITH GRIDDED POPULATION?
 pop_path = "D:\\CMIP6_data\\Pop\\"
@@ -41,7 +40,7 @@ fractionCountry[fractionCountry > 1.0] = 0.0
 # Change Longitude from -180 to 180 to 0 to 360 for ease of computation
 fractionCountry = np.concatenate(
     [
-        fractionCountry[:, :, len(longitude) // 2 :],
+        fractionCountry[:, :, len(longitude) // 2:],
         fractionCountry[:, :, : len(longitude) // 2],
     ],
     axis=2,
@@ -51,28 +50,36 @@ longitude = np.arange(0.25, 360, 0.5)
 ####################################################################################################
 
 ssps = ["ssp1", "ssp2", "ssp3"]
-age_groups = [5, 7, 9]
-age_group_names = np.array(["25+", "65+", "80+"], dtype=object)
+age_groups = [5, 7, 9, 10]
+age_group_names = np.array(["25+", "65+", "80+", "all"], dtype=object)
 
-years = np.arange(2010, 2110, 10)
+# (2010 - 1950) / 5 = 12
+years = np.arange(12, 12 + 2 * 10, 2)
+year_names = np.arange(1950, 2105, 5)
 
 for ssp in range(len(ssps)):
 
     data = country_path + country_file[ssp]
     country_pop_ds = xr.open_dataset(data)
-    country_pop_all_age_values = country_pop_ds.data_vars["Population"].values
     country_pop_ds.close()
+
+    country_pop_values_all_ages = np.zeros((31, 11, 193))
+    country_pop_values_all_ages[:, 0:10, :] = country_pop_ds.data_vars["Population"].values
+    country_pop_values_all_ages[:, 10, :] = np.sum(country_pop_values_all_ages, axis=1)
 
     # Select age groups
     # Shape: Year, Age Group, Country ID
-    country_pop_values = country_pop_all_age_values[:, [1, 3, 6, 8], :]
+    country_pop_values = country_pop_values_all_ages[:, age_groups, :]
 
     # Total Population of a Country:
-    country_pop_sum = np.sum(country_pop_values, axis=1)
+    country_pop_sum = country_pop_values[:, -1, :]
 
-    for year in range(len(years)):
+    for year_index in years:
+
+        cur_year = year_names[year_index]
+
         # Import gridded population data
-        grid_pop_file_name = f"{ssps[ssp]}_tot_{str(years[year])}.nc"
+        grid_pop_file_name = f"{ssps[ssp]}_tot_{cur_year}.nc"
         grid_pop_ds = xr.open_dataset(f"{pop_path}\\{ssps[ssp]}\\{grid_pop_file_name}")
         grid_pop_values = grid_pop_ds.data_vars["population"].values
         grid_pop_ds.close()
@@ -81,16 +88,16 @@ for ssp in range(len(ssps)):
         pop_array = np.zeros(shape=(len(latitude), len(longitude), len(age_groups)))
         for age in range(len(age_groups)):
 
-            for i in range(len(country_pop_values[year][age])): # Number of countries
+            for j in range(len(country_pop_values[year_index][age])):  # Number of countries
 
                 ratio = (
-                    country_pop_values[year, age, i] / country_pop_sum[year, i]
-                    if country_pop_sum[year, i] != 0
+                    country_pop_values[year_index, age, j] / country_pop_sum[year_index, j]
+                    if country_pop_sum[year_index, j] != 0
                     else 0
                 )
 
                 pop_array[:, :, age] += (
-                    fractionCountry[i, :, :] * grid_pop_values[:, :] * ratio
+                        fractionCountry[j, :, :] * grid_pop_values[:, :] * ratio
                 )
 
         # Create Dataset
@@ -110,7 +117,7 @@ for ssp in range(len(ssps)):
 
         # Output
         os.makedirs(f"{output_path}\\{ssps[ssp]}", exist_ok=True)
-        ds.to_netcdf(f"{output_path}\\{ssps[ssp]}\\{str(years[year])}.nc")
+        ds.to_netcdf(f"{output_path}\\{ssps[ssp]}\\{cur_year}.nc")
         ds.close()
 
-        print(f"{datetime.now()} DONE: {ssps[ssp]}, {years[year]}")
+        print(f"{datetime.now()} DONE: {ssps[ssp]}, {cur_year}")
