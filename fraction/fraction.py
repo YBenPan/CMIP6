647a7052f34bbe5e15343763a5116ad3b91e0926
YBenPan <ybenp8104@outlook.com>
2022-03-23 21:30:54 -0400

    Creates separate functions for subnational and combined outputs

--- fraction/fraction.py
+++ fraction/fraction.py
@@ -3,84 +3,97 @@ import pandas as pd
 import xarray as xr
 import math
 
-#### COUNTRY-SPECIFIC INPUT ####
-header_name = "ADM1_EN"
-country = "kenya"
-country_long_name = "Kenya"
-# left lon, top lat, state names, shape area
-cols = [0, 1, 2, 3]
-to_be_renamed = {
-    "Homa Bay": "HomaBay",
-    "Taita Taveta": "TaitaTaveta",
-    "Tana River": "TanaRiver",
-    "Tharaka Nithi": "TharakaNithi",
-    "Trans Nzoia": "TransNzoia",
-    "Uasin Gishu": "UasinGishu",
-    "West Pokot": "WestPokot",
-    "Tharaka-Nithi": "TharakaNithi",
-}
-#################################
+####################################################################################################
+#### CREATES SUBNATIONAL FRACTION FILE FROM GRIDDED SHAPEFILES
+#### CREATES COMBINED FRACTION FILE FROM SUBNATIONAL AND NATIONAL FRACTION FILES
+####################################################################################################
 
-#### PATH SETTINGS ####
-area_path = "D:/CMIP6_data/fraction/"
-area_file = f"{country}_state_gridded.csv"
-output_path = "D:/CMIP6_data/fraction/"
-output_file = f"{country}_state_fraction_0.5x0.5.nc"
-#######################
+countries = ["brazil", "indonesia", "japan", "kenya", "mexico", "us", "uk"]
 
-lon_start = -179.75
-lat_start = -89.75
-earth_radius2 = 6.37e6 ** 2
-deg2rad = math.pi / 180.0
-dx = 0.5
-dy = 0.5
 
-data = pd.read_csv(area_path + area_file, usecols=cols)
-states = data[header_name].values
-states = sorted(list(set(states)))  # remove duplicates
-# print(states)
+def subnational_output():
+    """
+    Outputs subnational fraction file
+    """
 
-# Initialize
-fraction = np.zeros((len(states), 360, 720))
+    #### COUNTRY-SPECIFIC INPUT ####
+    header_name = "ADM1_EN"
+    country = "kenya"
+    country_long_name = "Kenya"
+    # left lon, top lat, state names, shape area
+    cols = [0, 1, 2, 3]
+    to_be_renamed = {
+        "Homa Bay": "HomaBay",
+        "Taita Taveta": "TaitaTaveta",
+        "Tana River": "TanaRiver",
+        "Tharaka Nithi": "TharakaNithi",
+        "Trans Nzoia": "TransNzoia",
+        "Uasin Gishu": "UasinGishu",
+        "West Pokot": "WestPokot",
+        "Tharaka-Nithi": "TharakaNithi",
+    }
+    #################################
 
-for i in range(len(states)):
-    state = states[i]
-    wk = data[data[header_name] == state].to_xarray()
+    #### PATH SETTINGS ####
+    area_path = "D:/CMIP6_data/fraction/"
+    area_file = f"{country}_state_gridded.csv"
+    output_path = "D:/CMIP6_data/fraction/"
+    output_file = f"{country}_state_fraction_0.5x0.5.nc"
+    #######################
 
-    for j in range(len(wk.index)):
-        lat = wk["top"][j].values - 0.25
-        lon = wk["left"][j].values + 0.25
-        lat_ind = int((lat - lat_start) / dy)
-        lon_ind = int((lon - lon_start) / dx)
+    lon_start = -179.75
+    lat_start = -89.75
+    earth_radius2 = 6.37e6 ** 2
+    deg2rad = math.pi / 180.0
+    dx = 0.5
+    dy = 0.5
 
-        area = wk["SHAPEAREA"][j].values
-        grid_area = (
-            earth_radius2 * math.cos(lat * deg2rad) * (dx * deg2rad) * (dy * deg2rad)
-        )
-        fraction[i, lat_ind, lon_ind] = area / grid_area
+    data = pd.read_csv(area_path + area_file, usecols=cols)
+    states = data[header_name].values
+    states = sorted(list(set(states)))  # remove duplicates
+    # print(states)
 
-fraction[:, :, :] = fraction[:, :, :] / np.nanmax(
-    fraction[:, :, :]
-)  # almost negligible normalization. max = 1.008
+    # Initialize
+    fraction = np.zeros((len(states), 360, 720))
 
-# Create output arrays
-lat_arr = np.arange(-89.75, 90.25, 0.5)
-lon_arr = np.arange(-179.75, 180.25, 0.5)
-output_states = [x.replace(u'\xa0', '') for x in states]
-output_states = [to_be_renamed[x] if x in to_be_renamed else x for x in output_states]
-print(output_states)
+    for i in range(len(states)):
+        state = states[i]
+        wk = data[data[header_name] == state].to_xarray()
 
-# Output as netCDF
-ds = xr.Dataset(
-    data_vars=dict(
-        fractionState=(["state", "lat", "lon"], fraction),
-    ),
-    coords=dict(
-        state=(["state"], output_states),
-        lat=(["lat"], lat_arr),
-        lon=(["lon"], lon_arr),
-    ),
-    attrs=dict(description=f"{country_long_name} state fractions on a 0.5x0.5 deg grid"),
-)
-ds.to_netcdf(output_path + output_file)
-ds.close()
+        for j in range(len(wk.index)):
+            lat = wk["top"][j].values - 0.25
+            lon = wk["left"][j].values + 0.25
+            lat_ind = int((lat - lat_start) / dy)
+            lon_ind = int((lon - lon_start) / dx)
+
+            area = wk["SHAPEAREA"][j].values
+            grid_area = (
+                earth_radius2 * math.cos(lat * deg2rad) * (dx * deg2rad) * (dy * deg2rad)
+            )
+            fraction[i, lat_ind, lon_ind] = area / grid_area
+
+    fraction[:, :, :] = fraction[:, :, :] / np.nanmax(
+        fraction[:, :, :]
+    )  # almost negligible normalization. max = 1.008
+
+    # Create output arrays
+    lat_arr = np.arange(-89.75, 90.25, 0.5)
+    lon_arr = np.arange(-179.75, 180.25, 0.5)
+    output_states = [x.replace(u'\xa0', '') for x in states]
+    output_states = [to_be_renamed[x] if x in to_be_renamed else x for x in output_states]
+    print(output_states)
+
+    # Output as netCDF
+    ds = xr.Dataset(
+        data_vars=dict(
+            fractionState=(["state", "lat", "lon"], fraction),
+        ),
+        coords=dict(
+            state=(["state"], output_states),
+            lat=(["lat"], lat_arr),
+            lon=(["lon"], lon_arr),
+        ),
+        attrs=dict(description=f"{country_long_name} state fractions on a 0.5x0.5 deg grid"),
+    )
+    ds.to_netcdf(output_path + output_file)
+    ds.close()
