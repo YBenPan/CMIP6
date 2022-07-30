0c62e4b134d06d13af29edf3405b52e2b1b64af0
YBenPan <ybenp8104@outlook.com>
2022-07-30 13:39:37 -0400

    Minor fixes

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -3,18 +3,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support.ui import WebDriverWait
 import time
 import shutil
 import os
 import pandas as pd
 
-diseases = ["COPD", "IHD", "LRI", "LC", "Stroke", "T2D"]
+diseases = ["Allcause", "COPD", "Dementia", "IHD", "LRI", "LC", "Stroke", "T2D"]
 # How to get disease id:
+# Important: do not type anything in the search bar of causes
 # Select dropdown item to inspect
 # Copy XPath of <li> element
-disease_ids = [126, 111, 12, 85, 112, 167]
-default_path = "/Users/benpan/Documents/Mortality"
+disease_ids = [1, 126, 152, 111, 12, 85, 112, 167]
+download_path = "D:\CMIP6_data\Mortality\All Age Mortality Projections_2040_dl"
+mort_path = "D:\CMIP6_data\Mortality\All Age Mortality Projections_2040"
 
 ages = [
     "25-29",
@@ -53,12 +55,12 @@ age_urls = [
 ]
 
 
-def main():
+def download():
     opts = Options()
     opts.headless = True
 
     # Set download path
-    prefs = {"download.default_directory": default_path}
+    prefs = {"download.default_directory": download_path}
     opts.add_experimental_option("prefs", prefs)
 
     # Create browser object
@@ -91,21 +93,21 @@ def main():
         for disease, disease_id in zip(diseases, disease_ids):
 
             # Cause dropdown
-            browser.find_element_by_xpath('//*[@id="s2id_autogen3"]').send_keys(
+            browser.find_element("xpath", '//*[@id="s2id_autogen3"]').send_keys(
                 Keys.SPACE
             )
-            cause_elem = browser.find_element_by_xpath(
-                f'//*[@id="select2-results-3"]/li[{disease_id}]'
+            cause_elem = browser.find_element(
+                "xpath", f'//*[@id="select2-results-3"]/li[{disease_id}]'
             )
             browser.execute_script("arguments[0].scrollIntoView();", cause_elem)
             cause_elem.click()
-            browser.find_element_by_xpath('//*[@id="s2id_autogen3"]').send_keys(
+            browser.find_element("xpath", '//*[@id="s2id_autogen3"]').send_keys(
                 Keys.SPACE
             )
 
             # FIXME: Select Age-specific
-            # browser.find_element_by_xpath('//*[@id="s2id_autogen18"]').send_keys(Keys.SPACE)
-            # year_elem = browser.find_element_by_xpath(
+            # browser.find_element("xpath", '//*[@id="s2id_autogen18"]').send_keys(Keys.SPACE)
+            # year_elem = browser.find_element("xpath",
             #     f'//*[@id="select2-results-18"]/li[9]'
             # )
             # browser.execute_script("arguments[0].scrollIntoView();", year_elem)
@@ -116,34 +118,37 @@ def main():
             time.sleep(2)
 
             # Click on download button
-            dl_elem = browser.find_element_by_xpath(
-                '//*[@id="header-actions-download"]'
+            dl_elem = browser.find_element(
+                "xpath", '//*[@id="header-actions-download"]'
             )
             dl_elem.click()
 
             # Click on "Chart data (csv)" button to download csv files
-            csv_dl_elem = browser.find_element_by_xpath(
-                '//*[@id="vizhub-action-download-content"]/span[2]'
+            csv_dl_elem = browser.find_element(
+                "xpath", '//*[@id="vizhub-action-download-content"]/span[2]'
             )
             csv_dl_elem.click()
 
             time.sleep(2)
-            os.sync()
+            # os.sync()
 
-            default_file = os.path.join(default_path, "download.csv")
-            df = pd.read_csv(default_file)
+            download_file = os.path.join(download_path, "download.csv")
+            df = pd.read_csv(download_file)
             print(df)
 
-            # Open download.csv and change name
-            default_file = os.path.join(default_path, "download.csv")
-            output_path = os.path.join(default_path, str(year), disease)
+            download_file = os.path.join(download_path, "download.csv")
+            output_path = os.path.join(download_path, str(year), disease)
             os.makedirs(output_path, exist_ok=True)
             output_file = os.path.join(output_path, f"{age}.csv")
-            shutil.move(default_file, output_file)
+            shutil.move(download_file, output_file)
 
             print(f"Done: {age}, {disease}")
 
     browser.quit()
+
+
+def main():
+    download()
 
 
 if __name__ == "__main__":
